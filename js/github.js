ec92570a465e4f942ffc79a8d054e2789dadd7b9
MrFlyingbananas <mrflyingbananas@yahoo.com>
2018-02-21 00:11:45 -0600

    Random sort of github repos

--- js/github.js
+++ js/github.js
@@ -1,36 +1,50 @@
 jQuery.githubUser = function(username, callback) {
-	jQuery.getJSON('https://api.github.com/users/'+username+'/repos?callback=?',callback)
+	jQuery.getJSON(
+		"https://api.github.com/users/" +
+			username +
+			"/repos?access_token=c397d1db88bb3c9249f5&callback=?",
+		callback
+	);
 };
 
 jQuery.fn.loadRepositories = function(username) {
-	this.html("<span>Querying GitHub for " + username +"'s repositories...</span>");
+	this.html(
+		"<span>Querying GitHub for " + username + "'s repositories...</span>"
+	);
 
 	var target = this;
 	$jq2.githubUser(username, function(data) {
 		var repos = data.data; // JSON Parsing
-		//sortByName(repos);
 		console.log(repos);
-		var list = $('<dl/>');
+		sortByName(repos);
+		var list = $("<dl/>");
 		target.empty().append(list);
 		stop = repos.length - 6;
-		if(stop < 0)
-			stop = 0;
-		for(i =  repos.length - 1; i >= stop; i--){
+		if (stop < 0) stop = 0;
+		for (i = repos.length - 1; i >= stop; i--) {
 			repo = repos[i];
-			if (repo.name != (username.toLowerCase()+'.github.com')) {
-				list.append('<dt><a href="'+ (repo.homepage?repo.homepage:repo.html_url) +'">' + repo.name.trim() + '</a> <em>'+(repo.language?('('+repo.language+')'):'')+'</em></dt>');
-				if(repo.description != null){
-					list.append('<dd>' + repo.description +'</dd>');
-				}else{
-					list.append('<dd>' + "GitHub Project" +'</dd>');
+			if (repo.name != username.toLowerCase() + ".github.com") {
+				list.append(
+					'<dt><a href="' +
+						(repo.homepage ? repo.homepage : repo.html_url) +
+						'">' +
+						repo.name.trim() +
+						"</a> <em>" +
+						(repo.language ? "(" + repo.language + ")" : "") +
+						"</em></dt>"
+				);
+				if (repo.description != null) {
+					list.append("<dd>" + repo.description + "</dd>");
+				} else {
+					list.append("<dd>" + "GitHub Project" + "</dd>");
 				}
 			}
 		}
 	});
 
 	function sortByName(repos) {
-		repos.sort(function(a,b) {
-			return a.name - b.name;
+		repos.sort(function(a, b) {
+			return 0.5 - Math.random();
 		});
 	}
 };
